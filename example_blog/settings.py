c8d0321a07acab511c2ccbb7331325413ea2db42
fki <freekey@yandex.ru>
2016-06-08 21:17:09 +0300

    add static_root

--- example_blog/settings.py
+++ example_blog/settings.py
@@ -29,6 +29,8 @@ INSTALLED_APPS = [
     'django.contrib.sites',
     'django.contrib.sitemaps',
     'django_markdown',
+    'bootstrapform',
+    'allauth_bootstrap',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
@@ -116,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 EMAIL_HOST = 'smtp.yandex.ru'
 EMAIL_HOST_USER = 'freekey@yandex.ru'
