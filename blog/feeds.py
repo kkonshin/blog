8e0a679b1221abad1f00e745cffe505847175f48
fki <freekey@yandex.ru>
2016-06-21 15:39:54 +0300

    fixed RSS feed

--- blog/feeds.py
+++ blog/feeds.py
@@ -1,15 +1,15 @@
 from django.contrib.syndication.views import Feed
 from django.template.defaultfilters import truncatewords
-from .models import Post
+from blog.models import Post
 
 
 class LatestPostsFeed(Feed):
     title = 'Мой блог'
-    link = '/'
+    link = '/feed/'
     description = 'Новые записи в моем блоге'
 
     def items(self):
-        return Post.published.all()[:10]
+        return Post.published.all()[:5]
 
     def item_title(self, item):
         return item.title
--- blog/urls.py
+++ blog/urls.py
@@ -2,10 +2,12 @@ from django.conf.urls import url
 from . import views
 from .feeds import LatestPostsFeed
 
+# Порядок урлов очень важен. Понять.
+
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
+    url(r'^feed/$', LatestPostsFeed(), name='post_feed'),
     url(r'^(?P<post>[-\w]+)/$', views.post_detail, name='post_detail'),
     url(r'^(?P<post_id>\d+)/share/$', views.post_share, name='post_share'),
     url(r'^tag/(?P<tag_slug>[-\w]+)/$', views.post_list, name='post_list_by_tag'),
-    url(r'^feed/$', LatestPostsFeed(), name='post_feed'),
 ]
